4278682e19348af9cbd7630afd151f8cb381e58d
habakkukyong <habakkukyong@gmail.com>
2019-01-10 13:50:50 +0000

    Gave default value to dock capacity

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,6 +2,7 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :docked_bikes
+  DEFAULT_CAPACITY = 20
 
   def initialize
     @docked_bikes = []
@@ -20,7 +21,7 @@ class DockingStation
   private
 
   def full?
-    @docked_bikes.length >= 20
+    @docked_bikes.length >= DEFAULT_CAPACITY
   end
 
   def no_bikes?
