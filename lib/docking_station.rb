56fdd1645f06d98ac304f1932c435553434188e7
habakkukyong <habakkukyong@gmail.com>
2019-01-10 13:48:24 +0000

    Refactor applying single responsibility principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,22 @@ class DockingStation
   end
 
   def release_bike
-    fail "There are no bikes" if @docked_bikes.empty?
+    fail "There are no bikes" if no_bikes?
     @docked_bikes.pop
   end
 
   def dock(bike)
-    fail "Station is full" if @docked_bikes.length >= 20
+    fail "Station is full" if full?
     @docked_bikes.push(bike)
   end
 
+  private
+
+  def full?
+    @docked_bikes.length >= 20
+  end
+
+  def no_bikes?
+    @docked_bikes.empty?
+  end
 end
